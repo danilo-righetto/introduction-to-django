32a065d0fdfb996e10dbfa4bbe8e406809a3871d
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-25 10:58:56 -0300

    Links, urls e views

--- receitas/urls.py
+++ receitas/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('receita', views.receita, name='receita')
+
 ]
