81f2b8f3e3c4c61db9a9aab5f51b703e8b177734
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-26 22:40:48 -0300

    Exibindo dados do banco

--- receitas/views.py
+++ receitas/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
+from .models import Receitas
 
 # Create your views here.
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete',
-        4: 'Bolo de Chocolate'
-    }
+    receitas = Receitas.objects.all()
+
     dados = {
         'nome_das_receitas': receitas
     }
-    return render(request, 'index.html', {
-        'nome_da_receita': 'Sorvete'
-    })
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
